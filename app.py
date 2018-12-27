96b0c2cea450e7e49e5e3c4003da58b65c9b503c
vaidik1508 <36123459+vaidik1508@users.noreply.github.com>
2018-12-27 14:47:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template
+import os
+app=Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello Word"
+    
+    
+  
+@app.route('/profile/<user>')
+def profile(user=None):
+    return render_template("profile.html",name=user)
+    
+    
+    
+if __name__ == "__main__":
+    app.debug = True
+    port = int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0',port = port)
+    #app.run()
